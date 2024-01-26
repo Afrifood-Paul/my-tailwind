bfb8e5562dc1e642d3e1141ba183bfb1633bbb68
Afrifood-Paul <docpaul2016@gmail.com>
2024-01-26 13:30:41 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      textDecorationThickness: {
+        3: '3px',
+      }
+    },
+  },
+  plugins: [],
+}
+
